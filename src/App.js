69a9fdbd9e1dfecb6386488e409105837cf27050
Bartkooo <bartosz.plochocki@gmail.com>
2023-09-28 16:03:49 +0200

    colors

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import Hover from "react-3d-hover"
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [number, setNumber] = useState('**** **** **** ****')
   const [name, setName] = useState('Name Surname')
   const [date, setDate] = useState('00-00')
+  const [color, setColor] = useState('black')
+
+
+  useEffect(() => {
+    document.getElementById('card').style.backgroundColor = color
+  }, [color])
 
   return (
     <div className="app center">
       <div className="container">
         <div className="form">
           <input placeholder="cardholder name" className="form-input" onChange={e => setName(e.target.value)} />
-          <input placeholder="card number" className="form-input" onChange={e => setNumber(e.target.value)} />
+          <input placeholder="card number" className="form-input" maxLength={16} onChange={e => setNumber(e.target.value.toString().replace(/\d{4}(?=.)/g, '$& '))} />
           <input type="date" className="form-input" onChange={e => setDate(e.target.value)} />
-          <div className="form-color">
+          <form className="form-color">
             <h3>Choose color:</h3>
             <div>
-              <input type="radio" name="card-color" />
+              <input type="radio" name="card-color" value={'black'} onChange={e => setColor(e.target.value)} />
               <label>Black</label>
             </div>
             <div>
-              <input type="radio" name="card-color" />
+              <input type="radio" name="card-color" value={'blue'} onChange={e => setColor(e.target.value)} />
               <label>Blue</label>
             </div>
             <div>
-              <input type="radio" name="card-color" />
+              <input type="radio" name="card-color" value={'red'} onChange={e => setColor(e.target.value)} />
               <label>Red</label>
             </div>
-          </div>
+          </form>
         </div>
 
         <div className="center">
